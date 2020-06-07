b63e36758313f4abc458cfa09c29d06977909515
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-06-07 20:08:37 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,11 +1,12 @@
 require 'pry'
 
 def my_all?(collection)
-counter =  0
-returnvalues = []
+  counter =  0
+  returnvalues = []
 
-while counter < collection.length
-returnvalues <<  yield(collection[counter])
-counter += 1
-end
+  while counter < collection.length
+    returnvalues <<  yield(collection[counter])
+    counter += 1
+  end
+
 end
